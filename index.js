1b2ca168f939821d8518eb073a793bec13e2aa99
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-25 20:22:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    };
+
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(response => response.json())
+        .then(data => {
+            document.body.innerHTML += `<p>New user ID: ${data.id}</p>`;
+        })
+        .catch(error => {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+}
